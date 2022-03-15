c29b23e98899f5c2a0c2fbea6ae7bce2e50bd7e6
Deeno Gonzalez <deenogonzalez@swuser-129-8-235-158.swlan.csufresno.edu>
2022-03-15 15:23:22 -0700

    chapter_3_1

---
+++ 3_1.py
@@ -0,0 +1,40 @@
+
+
+"""
+Relational Operators
+<, <=, >, >=, !=, ==
+Logical Operators
+and, or, not
+"""
+
+# # true_or_false = "b" > "a"; #True 
+# true_or_false = "z" < "!"; #False
+# true_or_false = "z" < "!" and "b" > "a"; #False
+# true_or_false = "z" < "!" or "b" > "a"; #True
+
+# true_or_false = [3, 5] < [3, 7] #True
+
+# true_or_false = 3 in [3, 8] #True
+
+# true_or_false = not("z" < "!" or "b" > "a") #False
+
+# c = "abc"
+
+# true_or_false = c < "good" + "d"
+
+# print("true_or_false", true_or_false)
+
+# list1 = [6, 4, -5, 3.5]
+
+# print(list1)
+# list1.sort()
+
+# print(list1)
+
+# list2 = ["ha", "hi", 'B', '7']
+
+# print(list2)
+# list2.sort()
+# print(list2)
+
+# monarchs[-1][1] # ("George", 5)
