1e6222112f61d85569d9278479cb12f5b440e488
Deeno Gonzalez <deenogonzalez@swuser-129-8-232-153.swlan.csufresno.edu>
2022-04-19 14:41:59 -0700

    completed chapter 3

--- 3_3.py
+++ 3_3.py
@@ -59,11 +59,32 @@
 #     i += 1
 # print("In ", str(i), "years you will have a million dollars")
 
-list1 = []
-while True:
-    num = eval(input("Enter a nonnegative number: "))
-    if num == -1:
-        break # Immediately terminate the 
-    list1.append(num)
+# list1 = []
+# while True:
+#     num = eval(input("Enter a nonnegative number: "))
+#     if num == -1:
+#         break # Immediately terminate the 
+#     list1.append(num)
 
-print(list1)
+# print(list1)
+
+""""
+Display facts about the United States
+"""
+
+print("Enter a number from the menu to obtain a fact")
+print("about the United States or to exit the program. \n")
+print("1. Captial")
+print("2. National Bird")
+print("3. National Flower")
+print("4. Quit\n")
+while True:
+    num = int(input("Make a selection from the menu: "))
+    if num == 1:
+        print("Washington, DC is the captial of the United States. ")
+    elif num == 2:
+        print("The American Bald Eagle is the national bird. ")
+    elif num == 3:
+        print("The Rose is the national flower.")
+    elif num == 4:
+        break
